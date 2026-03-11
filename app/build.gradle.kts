5227a3ec57b2422643334323e3f1e4c46c43ea0c
pkab00 <pika_pikabu@vk.com>
2026-03-11 15:24:10 +0700

    Configuring Material Theme

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,28 +50,26 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material3.adaptive.navigation.suite)
-    implementation(libs.androidx.compose.material.icons.core)
+    implementation(libs.androidx.compose.ui.text.google.fonts)
 
-    dependencies {
-        // --- HILT ---
-        implementation(libs.hilt.android)
-        ksp(libs.hilt.android.compiler)
-        implementation(libs.androidx.hilt.navigation.compose)
+    // --- HILT ---
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.android.compiler)
+    implementation(libs.androidx.hilt.navigation.compose)
 
-        // --- RETROFIT ---
-        implementation(libs.retrofit)
-        implementation(libs.converter.kotlinx.serialization)
-        implementation(libs.kotlinx.serialization.json)
-        implementation(libs.logging.interceptor)
+    // --- RETROFIT ---
+    implementation(libs.retrofit)
+    implementation(libs.converter.kotlinx.serialization)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.logging.interceptor)
 
-        // --- ROOM ---
-        implementation(libs.androidx.room.runtime)
-        implementation(libs.androidx.room.ktx)
-        ksp(libs.androidx.room.compiler)
+    // --- ROOM ---
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 
-        // --- NAVIGATION ---
-        implementation(libs.androidx.navigation.compose)
-    }
+    // --- NAVIGATION ---
+    implementation(libs.androidx.navigation.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/vbshkn/ikollect/MainActivity.kt
+++ app/src/main/java/com/vbshkn/ikollect/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
 import androidx.compose.runtime.Composable
@@ -19,8 +19,12 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.PreviewScreenSizes
 import com.vbshkn.ikollect.presentation.theme.IKollectTheme
 
@@ -29,7 +33,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            IKollectTheme {
+            IKollectTheme(dynamicColor = false) {
                 IKollectApp()
             }
         }
@@ -39,7 +43,7 @@ class MainActivity : ComponentActivity() {
 @PreviewScreenSizes
 @Composable
 fun IKollectApp() {
-    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.HOME) }
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.ALBUMS) }
 
     NavigationSuiteScaffold(
         navigationSuiteItems = {
@@ -47,11 +51,11 @@ fun IKollectApp() {
                 item(
                     icon = {
                         Icon(
-                            it.icon,
-                            contentDescription = it.label
+                            imageVector = ImageVector.vectorResource(it.iconRes),
+                            contentDescription = stringResource(it.labelRes)
                         )
                     },
-                    label = { Text(it.label) },
+                    label = { Text(stringResource(it.labelRes)) },
                     selected = it == currentDestination,
                     onClick = { currentDestination = it }
                 )
@@ -60,7 +64,7 @@ fun IKollectApp() {
     ) {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
             Greeting(
-                name = "Android",
+                currentScreen = stringResource(currentDestination.labelRes),
                 modifier = Modifier.padding(innerPadding)
             )
         }
@@ -68,18 +72,29 @@ fun IKollectApp() {
 }
 
 enum class AppDestinations(
-    val label: String,
-    val icon: ImageVector,
+    val labelRes: Int,
+    val iconRes: Int,
 ) {
-    HOME("Home", Icons.Default.Home),
-    FAVORITES("Favorites", Icons.Default.Favorite),
-    PROFILE("Profile", Icons.Default.AccountBox),
+    ALBUMS(R.string.navigation_albums, R.drawable.ic_albums),
+    PHOTOCARDS(R.string.navigation_photocards, R.drawable.ic_photocards),
+    ACCOUNT(R.string.navigation_account, R.drawable.ic_account)
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(currentScreen: String, modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+        ) {
+            Text(
+                text = "Привет из $currentScreen!",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.displayMedium
+            )
+        }
+    }
 }
